663a7715e16ed899b638c32091f2540a95af19ba
rmzzx <rameezpathan31005@gmail.com>
2025-12-08 11:20:21 +0530

    netlify

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,12 @@
-// src/services/api.js
 import axios from "axios";
 
-console.log("Loaded API URL =", process.env.REACT_APP_API_URL);
-
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,  // MUST be replaced at build time
+  baseURL: process.env.REACT_APP_API_URL
 });
 
-// Add JWT token to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
